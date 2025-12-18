b7d908acccc3be9c24acb872acb4fa7bea412836
Garnet Grimm <garnetgrimm720@gmail.com>
2025-12-17 22:46:47 -0600

    update text

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,8 @@ use embedded_graphics::text::{Baseline, Text};
 use embedded_graphics::image::Image;
 
 use tinybmp::Bmp;
+use heapless::String;
+use core::fmt::Write;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -62,21 +64,22 @@ fn main() -> ! {
 
     let bmp_data = include_bytes!("../res/junimo.bmp");
     let bmp = Bmp::from_slice(bmp_data).unwrap();
-    let image = Image::new(&bmp, Point::new(25, 60));
+    let image = Image::new(&bmp, Point::new(150, 60));
     image.draw(&mut display).unwrap();
 
     epd.update_and_display_frame(&mut spi_dev, display.buffer(), &mut Delay)
         .unwrap();
 
     epd.set_lut(&mut spi_dev, &mut Delay, Some(RefreshLut::Quick)).unwrap();
-    let small_buffer = [Color::Black.get_byte_value(); 32]; //16x16
     for i in 0..8 {
-        let offset = i * 8 % 150;
-        epd.update_partial_frame(&mut spi_dev, &mut Delay, &small_buffer, 25 + offset, 25 + offset, 16, 16)
+        let mut data = String::<32>::new(); // 32 byte string buffer
+        core::write!(data, "time: 0:{:02}", i*10).unwrap();
+        draw_text(&mut display, &data, 3, 10);
+        epd.update_and_display_frame(&mut spi_dev, display.buffer(), &mut Delay)
             .unwrap();
-        epd.display_frame(&mut spi_dev, &mut Delay)
+        epd.update_and_display_frame(&mut spi_dev, display.buffer(), &mut Delay)
             .unwrap();
-        block_for(Duration::from_millis(100));
+        block_for(Duration::from_secs(10));
     }
 
     loop {
@@ -90,6 +93,7 @@ fn draw_text(display: &mut Display2in13, text: &str, x: i32, y: i32) {
     let text_style = MonoTextStyleBuilder::new()
         .font(&FONT_10X20)
         .text_color(Color::Black)
+        .background_color(Color::White)
         .build();
 
     Text::with_baseline(text, Point::new(x, y), text_style, Baseline::Top)
